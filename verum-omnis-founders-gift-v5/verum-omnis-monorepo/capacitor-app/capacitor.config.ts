d62d944e435b3b6a810ac7e45823e6b6c4d329f1
Liamhigh <36531077+Liamhigh@users.noreply.github.com>
2025-10-16 10:38:05 +0200

    change

--- verum-omnis-founders-gift-v5/verum-omnis-monorepo/capacitor-app/capacitor.config.ts
+++ verum-omnis-founders-gift-v5/verum-omnis-monorepo/capacitor-app/capacitor.config.ts
@@ -1,1 +1,13 @@
-export default { appId:'foundation.verumglobal.app', appName:'Verum Omnis', webDir:'www', server:{ url:'https://YOUR_HOST', cleartext:false } }
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'foundation.verumglobal.app',
+  appName: 'Verum Omnis',
+  webDir: 'www',
+  server: {
+	url: 'https://YOUR_HOST',
+	cleartext: false
+  }
+};
+
+export default config;
